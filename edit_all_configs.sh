9ac0c5ea10bbb6a6efce9134e0c280395382ba33
Shakari Wade <saturnfulcrum@ShakarisViewerOfCarnage>
2024-02-05 21:13:03 -0800

    Finished LSP changes added none-ls(null-ls fork).

---
+++ edit_all_configs.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+nvim init.lua lua/plugins/lsp-config.lua lua/plugins/lualine.lua lua/plugins/neo-tree.lua lua/plugins/none-ls.lua lua/plugins/onedark.lua lua/plugins/telescope.lua lua/plugins/treesitter.lua
