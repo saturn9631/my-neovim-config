6aaa7d899f7d89c3cb903d7a2577d8f320e87296
Shakari Wade <saturnfulcrum@ShakarisViewerOfCarnage>
2024-02-05 21:16:02 -0800

    Last commit did not record changes to lsp and telescope, this commit is to fix that. Last commits Message incase the commit failed: Finished LSP changes added none-ls(null-ls fork).

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,14 +3,22 @@ return {
 	'neovim/nvim-lspconfig',
 	dependencies = {
 		-- Automatically install LSPs to stdpath for neovim
-		{ 'williamboman/mason.nvim', config = true },
-		'williamboman/mason-lspconfig.nvim',
+		{ 'williamboman/mason.nvim',
+			config = true },
+		{'williamboman/mason-lspconfig.nvim'},
 
 		-- Useful status updates for LSP
 		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-		{ 'j-hui/fidget.nvim', opts = {} },
+		{ 'j-hui/fidget.nvim'},
 
 		-- Additional lua configuration, makes nvim stuff amazing!
-		'folke/neodev.nvim',
+		{'folke/neodev.nvim'},
 	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup({})
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+		vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+	end
   }
